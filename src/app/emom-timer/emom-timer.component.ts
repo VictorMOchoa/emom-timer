d09af0cfb37223da71b5c955730678bc89bb4e40
Victor Ochoa <ochoa.m.victor@gmail.com>
2025-05-05 00:10:18 -0700

    Created basic EMOM timer for a single round

--- src/app/emom-timer/emom-timer.component.ts
+++ src/app/emom-timer/emom-timer.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';  // Import CommonModule
 
 @Component({
   selector: 'app-emom-timer',
-  imports: [],
   templateUrl: './emom-timer.component.html',
-  styleUrl: './emom-timer.component.css'
+  imports: [CommonModule],  // Include CommonModule to use *ngIf
+  styleUrls: ['./emom-timer.component.css']
 })
 export class EmomTimerComponent {
+  secondsLeft = 60;
+  timerRunning = false;
+  intervalId: any;
 
+  startTimer() {
+    if (this.timerRunning) return; // Prevent multiple timers
+
+    this.timerRunning = true;
+    this.secondsLeft = 60;
+
+    this.intervalId = setInterval(() => {
+      this.secondsLeft--;
+
+      if (this.secondsLeft === 0) {
+        clearInterval(this.intervalId);
+        this.timerRunning = false;
+        // You can trigger a bell or next round logic here later
+      }
+    }, 1000);
+  }
 }
